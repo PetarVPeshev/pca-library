8ab61c4fb3e84468a55f4627efda35203efbb4b9
Petar Peshev <ppeshev@tudelft.nl>
2023-11-15 14:51:31 +0100

    added efficiency object and implemented time step functions
    implemented faster impressed current algorithm
    added time step algorithm for impressed current
    added efficiency object

--- PhotoConductor.m
+++ PhotoConductor.m
@@ -3,19 +3,19 @@ classdef PhotoConductor < handle
     %   Detailed explanation goes here
     
     properties (SetAccess = immutable)
-        rec_time (1,1) double
-        scat_time (1,1) double
-        rel_permit (1,1) double
-        electr_mass_coeff (1,1) double
-        absorp_coeff (1,1) double
-        dimens (1,3) double
+        rec_time            (1,1) double
+        scat_time           (1,1) double
+        rel_permit          (1,1) double
+        electr_mass_coeff   (1,1) double
+        absorp_coeff        (1,1) double
+        dimens              (1,3) double
     end
 
     properties (Dependent, SetAccess = protected)
         electr_eff_mass (1,1) double
-        permit (1,1) double
-        dc_carr_mobil (1,1) double
-        vol (1,1) double
+        permit          (1,1) double
+        dc_carr_mobil   (1,1) double
+        vol             (1,1) double
     end
     
     methods
@@ -29,12 +29,12 @@ classdef PhotoConductor < handle
             %   dimens [w_{x}, w_{y}, w_{z}] photoconductor dimensions [m m m]
 
             arguments
-                dimens (1,3) double {mustBePositive, mustBeReal}
-                rel_permit (1,1) double {mustBeGreaterThanOrEqual(rel_permit, 1), mustBeReal}
-                charact_times.rec_time (1,1) double {mustBePositive, mustBeReal}
-                charact_times.scat_time (1,1) double {mustBePositive, mustBeReal}
-                coeffs.electr_mass_coeff (1,1) double {mustBePositive, mustBeReal}
-                coeffs.absorp_coeff (1,1) double {mustBePositive, mustBeReal}
+                dimens                      (1,3) double {mustBePositive, mustBeReal}
+                rel_permit                  (1,1) double {mustBeGreaterThanOrEqual(rel_permit, 1), mustBeReal}
+                charact_times.rec_time      (1,1) double {mustBePositive, mustBeReal}
+                charact_times.scat_time     (1,1) double {mustBePositive, mustBeReal}
+                coeffs.electr_mass_coeff    (1,1) double {mustBePositive, mustBeReal}
+                coeffs.absorp_coeff         (1,1) double {mustBePositive, mustBeReal}
             end
 
             obj.dimens = dimens;
@@ -48,8 +48,7 @@ classdef PhotoConductor < handle
         function electr_eff_mass = get.electr_eff_mass(obj)
             %GET.ELECTR_MASS Summary of this method goes here
 
-            electr_eff_mass ...
-                = obj.electr_mass_coeff * get_phys_const('ElectronMass');
+            electr_eff_mass = obj.electr_mass_coeff * get_phys_const('ElectronMass');
         end
 
         function permit = get.permit(obj)
@@ -61,8 +60,7 @@ classdef PhotoConductor < handle
         function dc_carr_mobil = get.dc_carr_mobil(obj)
             %GET.DC_CARR_MOBIL Summary of this method goes here
 
-            dc_carr_mobil = get_phys_const('ElectronCharge') ...
-                * obj.scat_time / obj.electr_eff_mass;
+            dc_carr_mobil = get_phys_const('ElectronCharge') * obj.scat_time / obj.electr_eff_mass;
         end
 
         function vol = get.vol(obj)
@@ -71,90 +69,42 @@ classdef PhotoConductor < handle
             vol = obj.dimens(1) * obj.dimens(2) * obj.dimens(3);
         end
 
-        function reflectance = compute_reflectance(obj, inc_params)
+        function reflectance = compute_reflectance(obj, options)
             %COMPUTE_REFL_COEFF Summary of this method goes here
             %   inc_angle [\theta_{inc}] Incidence angle of laser beam at interface [deg]
             %   inc_rel_permit [\epsilon_{r}^{inc}] Relative permittivity of medium one (incidence medium) [-]
 
             arguments
                 obj
-                inc_params.inc_angle (1,1) double ...
-                    {mustBeGreaterThanOrEqual(inc_params.inc_angle, 0), ...
-                    mustBeLessThanOrEqual(inc_params.inc_angle, 90)} = 0
-                inc_params.inc_rel_permit (1,1) double ...
-                    {mustBeGreaterThanOrEqual(inc_params.inc_rel_permit, 1), mustBeReal} = 1
+                options.inc_angle       (1,1) double {mustBeInRange(options.inc_angle, 0, 90)} = 0
+                options.inc_rel_permit  (1,1) double {mustBeGreaterThanOrEqual(options.inc_rel_permit, 1), ...
+                                                      mustBeReal} = 1
             end
 
-            assert(inc_params.inc_angle == 0, 'Non-normal incidence is not implemented.');
+            assert(options.inc_angle == 0, 'Non-normal incidence is not implemented.');
 
-            inc_refr_idx = sqrt(inc_params.inc_rel_permit);
+            inc_refr_idx = sqrt(options.inc_rel_permit);
             pc_refr_idx = sqrt(obj.rel_permit);
 
-            reflectance = abs( (inc_refr_idx - pc_refr_idx) ...
-                / (inc_refr_idx + pc_refr_idx) ) ^ 2;
+            reflectance = abs( (inc_refr_idx - pc_refr_idx) / (inc_refr_idx + pc_refr_idx) ) ^ 2;
         end
 
-        function transmittance = compute_transmittance(obj, inc_params)
+        function transmittance = compute_transmittance(obj, options)
             %COMPUTE_TRANSM_COEFF Summary of this method goes here
             %   inc_angle [\theta_{inc}] Incidence angle of laser beam at interface [deg]
             %   inc_rel_permit [\epsilon_{r}^{inc}] Relative permittivity of medium one (incidence medium) [-]
 
             arguments
                 obj
-                inc_params.inc_angle (1,1) double ...
-                    {mustBeGreaterThanOrEqual(inc_params.inc_angle, 0), ...
-                    mustBeLessThanOrEqual(inc_params.inc_angle, 90)} = 0
-                inc_params.inc_rel_permit (1,1) double ...
-                    {mustBeGreaterThanOrEqual(inc_params.inc_rel_permit, 1), mustBeReal} = 1
+                options.inc_angle       (1,1) double {mustBeInRange(options.inc_angle, 0, 90)} = 0
+                options.inc_rel_permit  (1,1) double {mustBeGreaterThanOrEqual(options.inc_rel_permit, 1), ...
+                                                      mustBeReal} = 1
             end
 
-            reflectance = obj.compute_reflectance(...
-                inc_angle = inc_params.inc_angle, ...
-                inc_rel_permit = inc_params.inc_rel_permit);
+            reflectance = obj.compute_reflectance(inc_angle = options.inc_angle, ...
+                                                  inc_rel_permit = options.inc_rel_permit);
 
             transmittance = 1 - reflectance;
-        end
-
-        function absorp_effic = compute_absorp_effic(obj)
-            %COMPUTE_ABSORP_EFFIC Summary of this method goes here
-
-            absorp_effic = 1 - exp( - obj.dimens(3) / obj.absorp_coeff );
-        end
-
-        function so_effic = compute_so_effic(obj, laser)
-            %COMPUTE_SO_EFFIC Summary of this method goes here
-            %   laser Incident laser object
-
-            arguments
-                obj
-                laser (1,1) Laser
-            end
-
-            so_effic ...
-                = erf( obj.dimens(1) / (sqrt(8) * laser.spatial_std) ) ...
-                * erf( obj.dimens(2) / (sqrt(8) * laser.spatial_std) );
-        end
-
-        function opt_effic = compute_opt_effic(obj, laser, inc_params)
-            %COMPUTE_OPT_EFFIC Summary of this method goes here
-            %   laser Incident laser object
-            %   inc_angle [\theta_{inc}] Incidence angle of laser beam at interface [deg]
-            %   inc_rel_permit [\epsilon_{r}^{inc}] Relative permittivity of medium one (incidence medium) [-]
-
-            arguments
-                obj
-                laser (1,1) Laser
-                inc_params.inc_angle (1,1) double ...
-                    {mustBeGreaterThanOrEqual(inc_params.inc_angle, 0), ...
-                    mustBeLessThanOrEqual(inc_params.inc_angle, 90)} = 0
-                inc_params.inc_rel_permit (1,1) double ...
-                    {mustBeGreaterThanOrEqual(inc_params.inc_rel_permit, 1), mustBeReal} = 1
-            end
-
-            opt_effic = obj.compute_transmittance( ...
-                inc_angle = inc_params.inc_angle, ...
-                inc_rel_permit = inc_params.inc_rel_permit) ...
-                * obj.compute_absorp_effic * obj.compute_so_effic(laser);
         end
     end
 end
--- main.m
+++ main.m
@@ -1,6 +1,49 @@
-close all;
+% close all;
 clear;
 clc;
 
 [laser, photoconductor] = get_default_config();
-laser.plot_time_envelope(laser.pulse_half_pwr / 100);
+march_on = TimeStepAlgorithm(laser.get_time_vec(laser.pulse_half_pwr / 100));
+% laser.plot_time_envelope(laser.pulse_half_pwr / 100);
+% eff = Efficiency(laser, photoconductor, eff_opt = 0.35);
+
+antenna = PhotoConductiveAntenna(laser, photoconductor, march_on, eff_opt = 0.35);
+
+
+%% PARAMETERS
+dt = laser.pulse_half_pwr / 1000;
+t_vec = laser.get_time_vec(dt, max_t = 15 * laser.pulse_half_pwr);
+k = antenna.k_const;
+sigma_t = laser.temporal_std;
+tau_rec = photoconductor.rec_time;
+tau_s = photoconductor.scat_time;
+Vb = 30;
+
+%% h_{m}
+% v = NaN(1, length(t_vec));
+% v(1) = 0;
+
+i_impr = compute_i_impr(t_vec, k, Vb, tau_s, tau_rec, sigma_t);
+
+figure('Position', [250 250 700 400]);
+plot(t_vec * 1e12, i_impr, 'LineWidth', 2.0, ...
+    'DisplayName', ['time step, \delta_{t} = ' num2str(dt * 1e15) ' fs']);
+grid on;
+legend show;
+legend('location', 'bestoutside');
+xlabel('t / ps');
+ylabel('i(t) / A');
+title(['i_{impr} @ V_{b} = ' num2str(Vb) ' V, \tau_{rec} = ' num2str(tau_rec * 1e15) ...
+    ' fs, \tau_{s} = ' num2str(tau_s * 1e15) ' fs, \sigma_{t} = ' num2str(sigma_t * 1e15) ' fs']);
+
+%% FUNCTIONS
+function i_impr = compute_i_impr(t_vec, k, vb, tau_s, tau_rec, sigma_t)
+    m_max = length(t_vec);
+    dt = t_vec(2) - t_vec(1);
+
+    [N, M] = meshgrid(1 : 1 : m_max, 1 : 1 : m_max);
+
+    i_impr = tril( exp(- 0.5 * (t_vec(N) / sigma_t) .^ 2) .* exp(- (t_vec(M) - t_vec(N)) / tau_rec) ...
+        .* (1 - exp(- (t_vec(M) - t_vec(N)) / tau_s)) );
+    i_impr = k * vb * tau_s * dt * sum(i_impr, 2)';
+end
