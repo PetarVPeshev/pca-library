56f8bbd42a275cd61490535004a00a233c92146d
Petar Peshev <petar.peshev.dev@gmail.com>
2024-01-26 09:49:45 +0100

    added weight and admittance evaluation

--- dispersive_pca/dev_ga.m
+++ dispersive_pca/dev_ga.m
@@ -6,10 +6,8 @@ addpath('..\slots');
 addpath('..\utils');
 
 %% PARAMETERS
-% f = linspace(eps, 2, 4001) * 1e12;
-f = (0.05 : 0.005 : 2) * 1e12;
-% t = (-2 : 0.001 : 10) * 1e-12;
 t = linspace(-2, 10, 2001) * 1e-12;
+f = (0.05 : 0.005 : 2) * 1e12;
 % FEED GAP
 d_gap = 4.5 * 1e-6;
 % SLOT WIDTH
@@ -19,61 +17,71 @@ er_up = 11.7;  % medium 2
 er_dn = 1;     % medium 1
 % SLOT POSITIONS 
 dx = d_gap;
-x = (0 : 100 : 300) * 1e-6;
+x = (100 : 100 : 300) * 1e-6;
+% NUMBER OF POINTS
+Nf = length(f);
+Nt = length(t);
+Nx = length(x);
 
 %% SLOT OBJECT
 slot = SlotInDielectrics(d_gap, ws, er_up, er_dn);
 
-%% IMPEDANCE
-Zin = NaN(1, length(f));
-Z = NaN(length(x), length(f));
-for f_idx = 1 : 1 : length(f)
-    Zin(f_idx) = slot.compute_zin(f(f_idx));
-    Z(:, f_idx) = evaluate_Zx(x, f(f_idx), slot, dx)';
+%% WEIGHT
+% frequency domain
+W = NaN(1, Nf);
+Wm = W;
+for f_idx = 1 : 1 : Nf
+    W(f_idx) = slot.compute_zin(f(f_idx));
+    Wm(f_idx) = evaluate_Zx(0, f(f_idx), slot, dx);
 end
+W = (1 ./ W) .^ 2;
+Wm = (1 ./ Wm) .^ 2;
 
-Zplt = NaN(length(x) + 1, length(f));
-Zplt(1, :) = Zin;
-Zplt(2 : length(x) + 1, :) = Z;
+% time domain
+w = 2 * real(eval_IFT(t, f, W));
+wx = 2 * real(eval_IFT(t, f, Wm));
 
-ColorLegend = cell(1, 5);
-ColorLegend(1) = {'Z_{in}'};
-for x_idx = 2 : 1 : length(x) + 1
-    ColorLegend(x_idx) = cellstr(['Z_{x}, x = ' num2str(x(x_idx - 1) * 1e6) ' \mum']);
-end
-LineLegend = {'\Re', '\Im'};
-Location = 'bestoutside';
-LineWidth = 1.5;
-Color = ["#0072BD", "#EDB120", "#7E2F8E", "#77AC30", "#A2142F"];
-Position = [680 558 700 420];
-XLabel = 'f [THz]';
-YLabel = 'Z [\Omega]';
-Title = ['@ \Delta = ' num2str(d_gap * 1e6) ' \mum, d_{x} = ' num2str(dx * 1e6) ' \mum'];
-YLim = [-20 80];
+figure('Position', [680 558 700 420]);
 
-plot_two(f * 1e-12, real(Zplt), imag(Zplt), 'ColorLegend', ColorLegend, 'LineLegend', LineLegend, ...
-    'ColorLocation', Location, 'LineLocation', Location, 'LineWidth', LineWidth, 'Color', ...
-    Color, 'XLabel', XLabel, 'YLabel', YLabel, 'Title', Title, 'YLim', YLim, 'Position', Position);
+plot(t * 1e12, w, 'Color', "#0072BD", 'LineWidth', 1.5, 'DisplayName', 'w');
+hold on;
+plot(t * 1e12, wx, '--', 'Color', "#EDB120", 'LineWidth', 1.5, 'DisplayName', 'w_{x}');
+
 grid on;
+ylim([min(w) max(w)]);
+legend('location', 'bestoutside');
+
+xlabel('t [ps]');
+ylabel('w [S^{2}]');
+title(['@ w_{s} = ' num2str(ws * 1e6) ' \mum, \Delta = ' num2str(d_gap * 1e6) ' \mum, d_{x} = ' ...
+    num2str(dx * 1e6) ' \mum']);
+
+%% IMPEDANCE
+Zin = NaN(1, Nf);
+Zm = NaN(Nx, Nf);
+for f_idx = 1 : 1 : Nf
+    Zin(f_idx) = slot.compute_zin(f(f_idx));
+    Zm(:, f_idx) = evaluate_Zx(x, f(f_idx), slot, dx);
+end
 
 %% ADMITTANCE
-g = NaN(length(x) + 1, length(t));
-for x_idx = 1 : 1 : length(x)
-    if x_idx == 1
-        G = 1 ./ Zplt(x_idx, :);
-    else
-        G = Zplt(x_idx, :) ./ (Zplt(2, :) .^ 2);
-    end
-    g(x_idx, :) = 2 * real(eval_IFT(t, f, G));
+Hin = 1 ./ Zin;
+Hm = Zm .* Wm;
+
+% time domain
+hin = 2 * real(eval_IFT(t, f, Hin));
+hm = NaN(Nx, Nt);
+for x_idx = 1 : 1 : Nx
+    hm(x_idx, :) = 2 * real(eval_IFT(t, f, Hm(x_idx, :)));
 end
 
 figure('Position', [680 558 700 420]);
 
-plot(t * 1e12, g(1, :), 'LineWidth', 1.5, 'DisplayName', 'g_{in}');
+plot(t * 1e12, hin, 'LineWidth', 1.5, 'DisplayName', 'g_{in}');
 hold on;
-for x_idx = 3 : 1 : length(x) + 1
-    plot(t * 1e12, g(x_idx, :), 'LineWidth', 1.5, ...
-        'DisplayName', ['g_{x} / w_{x}, x = ' num2str(x(x_idx - 1) * 1e6) ' \mum']);
+for x_idx = 1 : 1 : Nx
+    plot(t * 1e12, hm(x_idx, :), 'LineWidth', 1.5, ...
+        'DisplayName', ['h_{m}, x = ' num2str(x(x_idx) * 1e6) ' \mum']);
     hold on;
 end
 hold off;
@@ -83,28 +91,7 @@ legend('location', 'bestoutside');
 
 xlabel('t [ps]');
 ylabel('g [S]');
-title(['w_{s} = ' num2str(ws * 1e6) ' \mum, \Delta = ' num2str(d_gap * 1e6) ' \mum, d_{x} = ' ...
-    num2str(dx * 1e6) ' \mum']);
-
-%% WEIGHT
-w = NaN(2, length(t));
-W = 1 ./ (Zplt(1, :) .^ 2);
-w(1, :) = 2 * real(eval_IFT(t, f, W));
-Wm = 1 ./ (Zplt(2, :) .^ 2);
-w(2, :) = 2 * real(eval_IFT(t, f, Wm));
-
-figure('Position', [680 558 700 420]);
-
-plot(t * 1e12, w(1, :), 'Color', "#0072BD", 'LineWidth', 1.5, 'DisplayName', 'w');
-hold on;
-plot(t * 1e12, w(2, :), '--', 'Color', "#EDB120", 'LineWidth', 1.5, 'DisplayName', 'w_{m}');
-
-grid on;
-legend('location', 'bestoutside');
-
-xlabel('t [ps]');
-ylabel('w [S^{2}]');
-title(['w_{s} = ' num2str(ws * 1e6) ' \mum, \Delta = ' num2str(d_gap * 1e6) ' \mum, d_{x} = ' ...
+title(['@ w_{s} = ' num2str(ws * 1e6) ' \mum, \Delta = ' num2str(d_gap * 1e6) ' \mum, d_{x} = ' ...
     num2str(dx * 1e6) ' \mum']);
 
 %% FUNCTIONS
@@ -112,27 +99,105 @@ function Zx = evaluate_Zx(x, f, slot, dx)
     c0 = get_phys_const('LightSpeed');
     k0 = 2 * pi * f / c0;
 
-    D = @(kx) slot.compute_D(kx, f, 'TopSheet');
+    Nx = length(x);
+
+    D_tt = @(kx) slot.compute_D(kx, f, 'TopSheet');
+    D_tb = @(kx) slot.compute_D(kx, f, 'BottomSheet');
+    D_bb = @(kx) compute_Dbb(slot, kx, f);
     F = @(kx) sinc(kx * slot.d_gap / (2 * pi));
     Fx = @(kx) sinc(- kx * dx / (2 * pi));
 
-    Zx = NaN(1, length(x));
-    for x_idx = 1 : 1 : length(x)
+    Zx = NaN(1, Nx);
+    for x_idx = 1 : 1 : Nx
         if x(x_idx) == 0
-            start_pt = (1000 * k0 + 1j * 0.01);
+            start_pt = - (1000 * k0 + 1j * 0.01);
             end_pt = - start_pt;
             waypts = [-(1 + 1j) (1 + 1j)] * 0.01;
 
-            integrand = @(kx) F(kx) .* Fx(kx) ./ D(kx);
+            integrand = @(kx) F(kx) .* Fx(kx) ./ D_tt(kx);
+            Zx(x_idx) = integral(integrand, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
         else
-            start_pt = - (0.01 + 1j * 50 * k0);
-            end_pt = k0 + 0.01 - 1j * 50 * k0;
-            waypts = [-(0.01 + 1j * 0.01) (0.01 + 1j * 0.01) (k0 + 1j * 0.01) (k0 + 0.01)];
+            k2 = k0 * sqrt(slot.er_up);
+            kxp = slot.find_kxp(f);
 
-            integrand = @(kx) F(kx) .* Fx(kx) .* exp(- 1j * kx * abs(x(x_idx))) ./ D(kx);
+            integrand_tt = @(kx) F(kx) .* Fx(kx) .* exp(- 1j * kx * abs(x(x_idx))) ./ D_tt(kx);
+            integrand_tb = @(kx) F(kx) .* Fx(kx) .* exp(- 1j * kx * abs(x(x_idx))) ./ D_tb(kx);
+            integrand_bb = @(kx) F(kx) .* Fx(kx) .* exp(- 1j * kx * abs(x(x_idx))) ./ D_bb(kx);
+
+            %% integration path 1
+%             % Top-Top sheet integral 1
+%             start_pt = - (0.01 + 1j * 50 * k0);
+%             end_pt = k0 + 0.01;
+%             waypts = [-(0.01 + 1j * 0.01) (0.01 + 1j * 0.01) (k0 + 1j * 0.01)];
+%             int_p1 = integral(integrand_tt, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
+% 
+%             % Top-Bottom sheet
+%             start_pt = k0 + 0.01;
+%             end_pt = k2 - 0.01;
+%             waypts = [(k0 - 1j * 0.01) (0.01 - 1j * 0.01) (0.01 - 1j * 50 * k0) (k2 - 0.01 - 1j * 50 * k0) ...
+%                       (k2 - 0.01 + 1j * (imag(kxp) - 1)) (real(kxp) - 1 + 1j * (imag(kxp) - 1)) ...
+%                       (real(kxp) - 1 + 1j * (imag(kxp) + 1)) (k2 - 0.01 + 1j * (imag(kxp) + 1))];
+%             int_p2 = integral(integrand_tb, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
+% 
+%             % Top-Top sheet integral 2
+%             start_pt = k2 - 0.01;
+%             end_pt = k2 + 0.01 - 1j * 50 * k0;
+%             waypts = [(k2 - 0.01 + 1j * 0.01) (k2 + 0.01 + 1j * 0.01)];
+%             int_p3 = integral(integrand_tt, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
+% 
+%             Zx(x_idx) = int_p1 + int_p2 + int_p3;
+
+            %% integration path 2
+%             % Top-Top sheet integral 1
+%             start_pt = - (0.01 + 1j * 50 * k0);
+%             end_pt = k0 + 0.01;
+%             waypts = [-(0.01 + 1j * 0.01) (0.01 + 1j * 0.01) (k0 + 1j * 0.01)];
+%             int_p1 = integral(integrand_tt, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
+% 
+%             % Top-Bottom sheet
+%             start_pt = k0 + 0.01;
+%             end_pt = k2 - 0.01;
+%             waypts = [(k0 + 0.01 - 1j * 50 * k0) (k2 - 0.01 - 1j * 50 * k0) ...
+%                       (k2 - 0.01 + 1j * (imag(kxp) - 1)) (real(kxp) - 1 + 1j * (imag(kxp) - 1)) ...
+%                       (real(kxp) - 1 + 1j * (imag(kxp) + 1)) (k2 - 0.01 + 1j * (imag(kxp) + 1))];
+%             int_p2 = integral(integrand_tb, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
+% 
+%             % Top-Top sheet integral 2
+%             start_pt = k2 - 0.01;
+%             end_pt = k2 + 0.01 - 1j * 50 * k0;
+%             waypts = [(k2 - 0.01 + 1j * 0.01) (k2 + 0.01 + 1j * 0.01)];
+%             int_p3 = integral(integrand_tt, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
+% 
+%             Zx(x_idx) = int_p1 + int_p2 + int_p3;
+
+            %% integration path 3
+            % Bottom-Bottom sheet integral
+            start_pt = k0 - 0.01 - 1j * 50 * k0;
+            end_pt = k0 - 0.01;
+            int_p1 = integral(integrand_bb, start_pt, end_pt) / (2 * pi);
+
+            % Top-Top sheet integral 1
+            start_pt = k0 - 0.01;
+            end_pt = k0 + 0.01;
+            waypts = [(k0 - 0.01 + 1j * 0.01) (k0 + 0.01 + 1j * 0.01)];
+            int_p2 = integral(integrand_tt, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
+
+            % Top-Bottom sheet integral
+            start_pt = k0 + 0.01;
+            end_pt = k2 - 0.01;
+            waypts = [(k0 + 0.01 - 1j * 50 * k0) (k2 - 0.01 - 1j * 50 * k0) ...
+                      (k2 - 0.01 + 1j * (imag(kxp) - 1)) (real(kxp) - 1 + 1j * (imag(kxp) - 1)) ...
+                      (real(kxp) - 1 + 1j * (imag(kxp) + 1)) (k2 - 0.01 + 1j * (imag(kxp) + 1))];
+            int_p3 = integral(integrand_tb, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
+
+            % Top-Top sheet integral 2
+            start_pt = k2 - 0.01;
+            end_pt = k2 + 0.01 - 1j * 50 * k0;
+            waypts = [(k2 - 0.01 + 1j * 0.01) (k2 + 0.01 + 1j * 0.01)];
+            int_p4 = integral(integrand_tt, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
+
+            Zx(x_idx) = int_p1 + int_p2 + int_p3 + int_p4;
         end
-        
-        Zx(x_idx) = integral(integrand, start_pt, end_pt, 'Waypoints', waypts) / (2 * pi);
     end
 end
 
@@ -155,25 +220,18 @@ function Zx = evaluate_Zx_analyt(x, f, slot, dx)
     Zx = - 1j * F * Fx * exp(- 1j * kxp * abs(x)) / Dp;
 end
 
-% OLD FUNCTIONS
-function D = compute_D(kx, f, ws, er_up, er_dn, sheet)
-    %COMPUTE_D Summary of this method goes here
-    %   Detailed explanation goes here
+function D = compute_Dbb(slot, kx, f)
     c0 = get_phys_const('LightSpeed');
     eta0 = get_phys_const('VacuumImpedance');
     
     k0 = 2 * pi * f / c0;
-    k1 = k0 * sqrt(er_dn);
-    k2 = k0 * sqrt(er_up);
+    k1 = k0 * sqrt(slot.er_dn);
+    k2 = k0 * sqrt(slot.er_up);
 
-    % Solution in top Riemann sheet
-    K1 = - 1j * sqrt(- k1 ^ 2 + kx .^ 2);
-    K2 = - 1j * sqrt(- k2 ^ 2 + kx .^ 2);
+    % Solution in bottom-bottom Riemann sheet
+    K1 = 1j * sqrt(- k1 ^ 2 + kx .^ 2);
+    K2 = 1j * sqrt(- k2 ^ 2 + kx .^ 2);
 
-    if strcmp(sheet, 'BottomSheet')
-        K2 = - K2;
-    end
-
-    D = (K1 .^ 2) .* compute_J0H02(K1 * ws / 4) + (K2 .^ 2) .* compute_J0H02(K2 * ws / 4);
+    D = (K1 .^ 2) .* compute_J0H02(K1 * slot.ws / 4) + (K2 .^ 2) .* compute_J0H02(K2 * slot.ws / 4);
     D = D / (2 * k0 * eta0);
 end
