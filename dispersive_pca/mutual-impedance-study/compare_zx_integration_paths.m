e60f1abc1a2ebf364e5d230fc08284d7d54a6350
Petar Peshev <petar.peshev.dev@gmail.com>
2024-02-05 18:53:10 +0100

    moved script for integral comparison

--- dispersive_pca/mutual-impedance-study/compare_zx_integration_paths.m
+++ dispersive_pca/mutual-impedance-study/compare_zx_integration_paths.m
@@ -2,7 +2,7 @@ close all;
 clear;
 clc;
 
-addpath('..\slots', '..\utils');
+addpath('..\..\slots', '..\..\utils');
 
 %% PARAMETERS
 f = linspace(eps, 2, 391) * 1e12;
@@ -17,7 +17,7 @@ er_dn = 1;     % medium 1
 dx = 0.5 * 1e-6;
 x  = linspace(0, 10, 101) * 1e-6;
 % xq = [0 0.5 1 1.5 2 2.5 3] * 1e-6;
-xq = [0.5 1] * 1e-6;
+xq = [0.5 1 2] * 1e-6;
 % NUMBER OF POINTS
 Nf = length(f);
 Nx = length(x);
@@ -33,91 +33,22 @@ for f_idx = 1 : 1 : Nf
     Zx_path3(:, f_idx) = evaluate_Zx_path3(x, f(f_idx), slot, dx);
 end
 
-xq_idx = find(ismember(x, xq));
-Color  = ["#0072BD", "#D95319", "#EDB120", "#7E2F8E", "#77AC30", "#4DBEEE", "#A2142F"];
+xq_idx   = find(ismember(x, xq));
+Position = [680 558 750 400];
+
 % REAL PART
-fig = figure('Position', [680 558 560 420]);
-
-AX1       = axes(fig);
-L         = gobjects(1, length(xq_idx));
-Legend    = cell(1, length(xq_idx));
-color_idx = 1;
-for x_idx = xq_idx
-    L(color_idx) = plot(f * 1e-12, real(Zx_full(x_idx, :)), 'LineWidth', 1.5, ...
-                  'Color', Color(color_idx), 'Parent', AX1);
-    hold on;
-             plot(f * 1e-12, real(Zx_path3(x_idx, :)), '--+', 'LineWidth', 1.5, ...
-                  'Color', Color(color_idx), 'MarkerIndices', 1 : 10 : Nf, 'Parent', AX1);
-    hold on;
-
-    Legend(color_idx) = cellstr(['x = ' num2str(x(x_idx) * 1e6) ' \mum']);
-    color_idx         = color_idx + 1;
-end
-    
-grid on;
-set(AX1, 'Box', 'off');
-LGND1 = legend(AX1, L, Legend, 'location', 'bestoutside');
-hold on;
-
-AX2 = copyobj(AX1, gcf);
-delete(get(AX2, 'Children'));
-H1 = plot(NaN, 'LineWidth', 1.5, 'Color', [0 0 0], 'Parent', AX2);
-H2 = plot(NaN, '--', 'LineWidth', 1.5, 'Color', [0 0 0], 'Parent', AX2);
-
-set(AX2, 'Color', 'none', 'XTick', [], 'Visible', 'off');
-LGND2 = legend([H1 H2], {'FULL', 'AROUND CUTS'}, 'location', 'bestoutside');
-LGND2.Position(1) = LGND1.Position(1);
-LGND2.Position(2) = LGND1.Position(2) - LGND2.Position(4) - 0.02;
-LGND2.Box   = 'off';
-LGND2.Color = 'none';
-hold off;
-
-xlabel('f [THz]');
-ylabel('\Re\{Z_{x}\} [\Omega]');
-title(['\Re @ w_{s} = ' num2str(ws * 1e6) ' \mum, \Delta = ' num2str(d_gap * 1e6) ' mum, d_{x} = ' ...
-       num2str(dx * 1e6) ' \mum']);
+YLim   = [0 80];
+YLabel = '\Re\{Z_{x}\} [\Omega]';
+Title  = ['\Re @ w_{s} = ' num2str(ws * 1e6) ' \mum, \Delta = ' num2str(d_gap * 1e6) ' mum, d_{x} = ' ...
+          num2str(dx * 1e6) ' \mum'];
+plot_comparison(f, real(Zx_full), real(Zx_path3), x, xq_idx, Position, YLim, YLabel, Title, true);
 
 % IMAG PART
-fig = figure('Position', [680 558 560 420]);
-
-AX1       = axes(fig);
-L         = gobjects(1, length(xq_idx));
-Legend    = cell(1, length(xq_idx));
-color_idx = 1;
-for x_idx = xq_idx
-    L(color_idx) = plot(f * 1e-12, imag(Zx_full(x_idx, :)), 'LineWidth', 1.5, ...
-                  'Color', Color(color_idx), 'Parent', AX1);
-    hold on;
-             plot(f * 1e-12, imag(Zx_path3(x_idx, :)), '--+', 'LineWidth', 1.5, ...
-                  'Color', Color(color_idx), 'MarkerIndices', 1 : 10 : Nf, 'Parent', AX1);
-    hold on;
-
-    Legend(color_idx) = cellstr(['x = ' num2str(x(x_idx) * 1e6) ' \mum']);
-    color_idx         = color_idx + 1;
-end
-
-grid on;
-set(AX1, 'Box', 'off');
-LGND1 = legend(AX1, L, Legend, 'location', 'bestoutside');
-hold on;
-
-AX2 = copyobj(AX1, gcf);
-delete(get(AX2, 'Children'));
-H1 = plot(NaN, 'LineWidth', 1.5, 'Color', [0 0 0], 'Parent', AX2);
-H2 = plot(NaN, '--', 'LineWidth', 1.5, 'Color', [0 0 0], 'Parent', AX2);
-
-set(AX2, 'Color', 'none', 'XTick', [], 'Visible', 'off');
-LGND2 = legend([H1 H2], {'FULL', 'AROUND CUTS'}, 'location', 'bestoutside');
-LGND2.Position(1) = LGND1.Position(1);
-LGND2.Position(2) = LGND1.Position(2) - LGND2.Position(4) - 0.02;
-LGND2.Box   = 'off';
-LGND2.Color = 'none';
-hold off;
-
-xlabel('f [THz]');
-ylabel('\Re\{Z_{x}\} [\Omega]');
-title(['\Re @ w_{s} = ' num2str(ws * 1e6) ' \mum, \Delta = ' num2str(d_gap * 1e6) ' mum, d_{x} = ' ...
-       num2str(dx * 1e6) ' \mum']);
+YLim   = [0 120];
+YLabel = '\Im\{Z_{x}\} [\Omega]';
+Title  = ['\Im @ w_{s} = ' num2str(ws * 1e6) ' \mum, \Delta = ' num2str(d_gap * 1e6) ' mum, d_{x} = ' ...
+          num2str(dx * 1e6) ' \mum'];
+plot_comparison(f, imag(Zx_full), imag(Zx_path3), x, xq_idx, Position, YLim, YLabel, Title, false);
 
 %% FUNCTIONS
 function Zx = evaluate_Zx(x, f, slot, dx)
@@ -215,4 +146,72 @@ function D = compute_Dbb(slot, kx, f)
 
     D = (K1 .^ 2) .* compute_J0H02(K1 * slot.ws / 4) + (K2 .^ 2) .* compute_J0H02(K2 * slot.ws / 4);
     D = D / (2 * k0 * eta0);
+end
+
+function fig = plot_comparison(f, y1, y2, x, xq_idx, Position, YLim, YLabel, Title, zoom_axes)
+    Color = ["#0072BD", "#D95319", "#EDB120", "#7E2F8E", "#77AC30", "#4DBEEE", "#A2142F"];
+    Nf    = length(f);
+
+    fig = figure('Position', Position);
+    
+    AX1       = axes(fig);
+    L         = gobjects(1, length(xq_idx));
+    Legend    = cell(1, length(xq_idx));
+    color_idx = 1;
+    for x_idx = xq_idx
+        L(color_idx) = plot(f * 1e-12, y1(x_idx, :), 'LineWidth', 1.5, ...
+                            'Color', Color(color_idx), 'Parent', AX1);
+        hold on;
+                       plot(f * 1e-12, y2(x_idx, :), '--+', 'LineWidth', 1.5, ...
+                            'Color', Color(color_idx), 'MarkerIndices', 1 : 20 : Nf, 'Parent', AX1);
+        hold on;
+    
+        Legend(color_idx) = cellstr(['x = ' num2str(x(x_idx) * 1e6) ' \mum']);
+        color_idx         = color_idx + 1;
+    end
+    
+    grid on;
+    ylim(YLim);
+    set(AX1, 'Box', 'off');
+    LGND1 = legend(AX1, L, Legend, 'location', 'bestoutside');
+    hold on;
+    
+    AX2 = copyobj(AX1, gcf);
+    delete(get(AX2, 'Children'));
+    H1 = plot(NaN, 'LineWidth', 1.5, 'Color', [0 0 0], 'Parent', AX2);
+    H2 = plot(NaN, '--', 'LineWidth', 1.5, 'Color', [0 0 0], 'Parent', AX2);
+    
+    set(AX2, 'Color', 'none', 'XTick', [], 'Visible', 'off');
+    LGND2 = legend([H1 H2], {'FULL', 'AROUND CUTS'}, 'location', 'bestoutside');
+    LGND2.Position(1) = LGND1.Position(1);
+    LGND2.Position(2) = LGND1.Position(2) - LGND2.Position(4) - 0.02;
+    LGND2.Box   = 'off';
+    LGND2.Color = 'none';
+    hold off;
+
+    if zoom_axes
+        AX3       = axes('Position', [0.38 0.22 0.35 0.35], 'Box', 'off');
+        color_idx = 1;
+        for x_idx = xq_idx
+            plot(f(f >= 1.4e12 & f <= 2e12) * 1e-12, y1(x_idx, f >= 1.4e12 & f <= 2e12), ...
+                 'LineWidth', 1.5, 'Color', Color(color_idx), 'Parent', AX3);
+            hold on;
+            plot(f(f >= 1.4e12 & f <= 2e12) * 1e-12, y2(x_idx, f >= 1.4e12 & f <= 2e12), '--+', ...
+                 'LineWidth', 1.5, 'Color', Color(color_idx), ...
+                 'MarkerIndices', 7 : 20 : length(f(f >= 1.4e12 & f <= 2e12)), 'Parent', AX3);
+            hold on;
+        
+            color_idx         = color_idx + 1;
+        end
+
+        grid on;
+        xlim([1.4 2]);
+        annotation('line', [0.73 0.75], [0.57 0.75], 'LineWidth', 0.75);
+        annotation('line', [0.38 0.555], [0.57 0.75], 'LineWidth', 0.75);
+        annotation('rectangle', [0.555 0.75 0.195 0.17], 'LineWidth', 0.75);
+    end
+    
+    xlabel('f [THz]', 'Parent', AX1);
+    ylabel(YLabel, 'Parent', AX1);
+    title(Title, 'Parent', AX1);
 end
